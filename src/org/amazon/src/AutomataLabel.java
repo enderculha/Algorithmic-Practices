4c83ceb7112f1a8e650d2383c2312666f81034de
Ender Culha <enderculha@gmail.com>
2018-02-15 00:13:31 +0300

    Commit automata challenge

--- src/org/amazon/src/AutomataLabel.java
+++ src/org/amazon/src/AutomataLabel.java
@@ -1,22 +1,68 @@
 package org.amazon.src;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-/**
- * Created by lenovo on 11.02.2018.
- */
 public class AutomataLabel {
     public static void main(String[] args) {
-        List<String> tagList = new ArrayList<>(Arrays.asList("made", "in", "spain"));
-        List<String> availableTagList = new ArrayList<>(Arrays.asList("en", "made", "in", "spain"));
-        List<Integer> output = new AutomataLabel().labelProduct(tagList, availableTagList);
+        List<String> tagList = new ArrayList<>(Arrays.asList("in", "the", "spain"));
+        List<String> availableTagList = new ArrayList<>(Arrays.asList("in","the", "spain"));
+        List<Integer> output = new AutomataLabel().labelProductEfficient(tagList, availableTagList);
 
         for(Integer i :output){
             System.out.print(i + " ");
         }
     }
 
-    public List<Integer> labelProduct(List<String> tagList, List<String>  availableTagList){
+    public List<Integer> labelProductEfficient(List<String> tagList, List<String>  availableTagList){
+        int[] store = new int[tagList.size()];
+        int startInd = 0;
+        int endInd = Integer.MAX_VALUE;
+        int minDistance = Integer.MAX_VALUE;
+        for(int i=0;i<availableTagList.size();i++){
+            String aTag = availableTagList.get(i);
+            for(int j=0; j<tagList.size();j++){
+                String tag = tagList.get(j);
+                if(aTag.equalsIgnoreCase(tag)){
+                    store[j] = store[j] + 1;
+                }
+            }
+            if(checkStoreArray(store)) {
+                if (i - startInd < minDistance) {
+                    minDistance = i - startInd;
+                    endInd=i;
+                    //remove from store if exist
+                    String extract = availableTagList.get(startInd);
+                    for (int j = 0; j < tagList.size(); j++) {
+                        String tag = tagList.get(j);
+                        if (extract.equalsIgnoreCase(tag)) {
+                            store[j] =store[j]-1;
+                        }
+                    }
+                    // increment startInd
+                    startInd++;
+                }else {
+                    break;
+                }
+
+            }
+        }
+
+        return new ArrayList<>(Arrays.asList(--startInd, endInd));
+    }
+
+    private boolean checkStoreArray(int[] store) {
+        for(int i=0;i<store.length;i++){
+            if(store[i]<1){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public List<Integer> labelProductBruteForce(List<String> tagList, List<String>  availableTagList){
         int startInd = 0;
         int endInd = 0;
         int distance = Integer.MAX_VALUE;
--- src/org/amazon/test/AutomataLabelTest.java
+++ src/org/amazon/test/AutomataLabelTest.java
@@ -1,7 +1,6 @@
 package org.amazon.test;
 
 import org.amazon.src.AutomataLabel;
-import org.hamcrest.core.IsEqual;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -12,55 +11,94 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
 
 
-/**
- * Created by lenovo on 12.02.2018.
- */
 public class AutomataLabelTest {
     final  AutomataLabel automataLabel = new AutomataLabel();
 
     @Test
-    public void TestCase1(){
+    public void labelProductBruteForceTestCase1(){
         List<String> tagList = new ArrayList<>(Arrays.asList("made", "in", "spain"));
         List<String> availableTagList = new ArrayList<>(Arrays.asList("made", "weather","Forecast" ,
                 "says", "that" , "made" , "rain" , "in" , "Spain" , "stays"));
-        List<Integer> actual = automataLabel.labelProduct(tagList, availableTagList);
+        List<Integer> actual = automataLabel.labelProductBruteForce(tagList, availableTagList);
         List<Integer> expected = new ArrayList<>(Arrays.asList(5,8));
 
         assertThat("Expected and actual should be equal", actual, equalTo(expected));
     }
 
     @Test
-    public void TestCase2(){
+    public void labelProductBruteForceTestCase2(){
         List<String> tagList = new ArrayList<>(Arrays.asList("2abc", "bcd", "cab"));
         List<String> availableTagList = new ArrayList<>(Arrays.asList("dbc", "2abc","cab" ,
                 "bcd", "bcb"));
-        List<Integer> actual = automataLabel.labelProduct(tagList, availableTagList);
+        List<Integer> actual = automataLabel.labelProductBruteForce(tagList, availableTagList);
         List<Integer> expected = new ArrayList<>(Arrays.asList(1,3));
 
         assertThat("Expected and actual should be equal", actual, equalTo(expected));
     }
 
     @Test
-    public void TestCase3(){
+    public void labelProductBruteForceTestCase3(){
         List<String> tagList = new ArrayList<>(Arrays.asList("in", "the", "spain"));
         List<String> availableTagList = new ArrayList<>(Arrays.asList("the", "spain","that" ,
                 "the", "rain" , "in" , "spain" , "stays" , "forecast" , "in", "the"));
-        List<Integer> actual = automataLabel.labelProduct(tagList, availableTagList);
+        List<Integer> actual = automataLabel.labelProductBruteForce(tagList, availableTagList);
         List<Integer> expected = new ArrayList<>(Arrays.asList(3,6));
 
         assertThat("Expected and actual should be equal", actual, equalTo(expected));
     }
 
     @Test
-    public void TestCase34(){
+    public void labelProductBruteForceTestCase4(){
         List<String> tagList = new ArrayList<>(Arrays.asList("in", "the", "spain"));
         List<String> availableTagList = new ArrayList<>(Arrays.asList("in", "the", "del", "ab"));
-        List<Integer> actual = automataLabel.labelProduct(tagList, availableTagList);
+        List<Integer> actual = automataLabel.labelProductBruteForce(tagList, availableTagList);
         List<Integer> expected = new ArrayList<>(Arrays.asList(0));
 
         assertThat("Expected and actual should be equal", actual, equalTo(expected));
     }
 
+    @Test
+    public void labelProductEfficientTestCase1(){
+        List<String> tagList = new ArrayList<>(Arrays.asList("made", "in", "spain"));
+        List<String> availableTagList = new ArrayList<>(Arrays.asList("made", "weather","Forecast" ,
+                "says", "that" , "made" , "rain" , "in" , "Spain" , "stays"));
+        List<Integer> actual = automataLabel.labelProductEfficient(tagList, availableTagList);
+        List<Integer> expected = new ArrayList<>(Arrays.asList(5,8));
+
+        assertThat("Expected and actual should be equal", actual, equalTo(expected));
+    }
+
+    @Test
+    public void labelProductEfficientTestCase2(){
+        List<String> tagList = new ArrayList<>(Arrays.asList("2abc", "bcd", "cab"));
+        List<String> availableTagList = new ArrayList<>(Arrays.asList("dbc", "2abc","cab" ,
+                "bcd", "bcb"));
+        List<Integer> actual = automataLabel.labelProductEfficient(tagList, availableTagList);
+        List<Integer> expected = new ArrayList<>(Arrays.asList(1,3));
+
+        assertThat("Expected and actual should be equal", actual, equalTo(expected));
+    }
+
+    @Test
+    public void labelProductEfficientTestCase3(){
+        List<String> tagList = new ArrayList<>(Arrays.asList("in", "the", "spain"));
+        List<String> availableTagList = new ArrayList<>(Arrays.asList("the", "spain","that" ,
+                "the", "rain" , "in" , "spain" , "stays" , "forecast" , "in", "the"));
+        List<Integer> actual = automataLabel.labelProductEfficient(tagList, availableTagList);
+        List<Integer> expected = new ArrayList<>(Arrays.asList(3,6));
+
+        assertThat("Expected and actual should be equal", actual, equalTo(expected));
+    }
+
+    @Test
+    public void labelProductEfficientTestCase4(){
+        List<String> tagList = new ArrayList<>(Arrays.asList("in", "the", "spain"));
+        List<String> availableTagList = new ArrayList<>(Arrays.asList("in", "the", "del", "ab"));
+        List<Integer> actual = automataLabel.labelProductEfficient(tagList, availableTagList);
+        List<Integer> expected = new ArrayList<>(Arrays.asList(0));
+
+        assertThat("Expected and actual should be equal", actual, equalTo(expected));
+    }
 
 
 
